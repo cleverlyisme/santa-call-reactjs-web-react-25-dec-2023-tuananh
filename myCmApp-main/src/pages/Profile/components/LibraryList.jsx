095633704ede146675f25986dfbe6d9296eaa104
Beo kutee <hunglcvph28989@fpt.edu.vn>
2024-01-19 11:56:05 +0700

    profile page library

--- myCmApp-main/src/pages/Profile/components/LibraryList.jsx
+++ myCmApp-main/src/pages/Profile/components/LibraryList.jsx
@@ -7,11 +7,18 @@ const LibraryList = ({ type, medias, handleView }) => {
           className="flex flex-col items-center md:flex-row gap-8 md:gap-12"
         >
           <div className="w-full h-[auto] md:w-[50%] md:h-[50%] rounded-lg overflow-hidden">
-            <img
-              src={type === "image" ? item.image : item.thumbnail}
+            {
+              type=="image"?
+              (<img
+              src={item.link_da_swap}
               alt={type}
               className="w-full h-full object-cover"
-            />
+              />)
+              :
+              (<video controls>
+                <source src={item.link_da_swap}  type="video/mp4"></source>
+              </video>)
+            }
           </div>
 
           <div className="truncate capitalize flex flex-col justify-center w-[fit-content] gap-2">
@@ -24,9 +31,6 @@ const LibraryList = ({ type, medias, handleView }) => {
               sapiente ipsam molestiae perferendis fugit, officia quo, obcaecati
               quae ut reiciendis quos odit distinctio, beatae commodi nihil
               iste? Explicabo, commodi repellat.
-            </span>
-            <span className="capitalize text-[16px] text-gray-400 mb-5">
-              Total 10 {type}s
             </span>
             <button
               className="capitalize bg-red-400 px-2 py-3 w-[100px] xl:w-[150px] rounded-lg text-white text-[14px] xl:text-[16px]"
